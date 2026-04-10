1521a87b4dbb9215540ae4bac392cdb94fd3bae6
Sujal Choudhary <chsujal8@gmail.com>
2026-04-10 16:49:44 +0530

    Initial Spring Boot project setup

---
+++ bud-repo-backend/src/main/java/com/sujal/bud_repo/BudRepoApplication.java
@@ -0,0 +1,13 @@
+package com.sujal.bud_repo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudRepoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudRepoApplication.class, args);
+	}
+
+}
---
+++ bud-repo-backend/src/test/java/com/sujal/bud_repo/BudRepoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sujal.bud_repo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudRepoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
