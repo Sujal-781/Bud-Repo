da2368d47489045a2a88acc095472004ec816486
Sujal Choudhary <chsujal8@gmail.com>
2026-04-11 15:05:17 +0530

    implemented file reading

---
+++ bud-repo-backend/src/main/java/com/sujal/bud_repo/ReadFiles.java
@@ -0,0 +1,32 @@
+package com.sujal.bud_repo;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
+class ReadFiles{
+    public static void main(String[] args) {
+        Path repoPath = Paths.get("/Users/sujalchoudhary/Desktop/Expensio");
+
+        try(Stream<Path> paths = Files.walk(repoPath)){
+            paths
+                    .filter(Files::isRegularFile)
+                    .filter(path -> path.toString().endsWith(".java"))
+                    .filter(path -> !isIgnore(path))
+                    .forEach(System.out::println);
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+    public static boolean isIgnore(Path path){
+        String p = path.toString();
+        return p.contains("target") ||
+                p.contains("build") ||
+                p.contains("node_modules") ||
+                p.contains(".git")||
+                p.startsWith(".");
+
+    }
+}
