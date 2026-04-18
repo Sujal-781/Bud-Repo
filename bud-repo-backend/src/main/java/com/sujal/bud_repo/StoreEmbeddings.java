6ced52d41b981d5f5d50ee4102ba1a06d480b5ae
Sujal Choudhary <chsujal8@gmail.com>
2026-04-19 00:57:15 +0530

    added logic for storing embeddings

---
+++ bud-repo-backend/src/main/java/com/sujal/bud_repo/StoreEmbeddings.java
@@ -0,0 +1,23 @@
+package com.sujal.bud_repo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StoreEmbeddings {
+    public static void main(String[] args) {
+        List<String[]> chunks = ChunkFiles.getChunks("/Users/sujalchoudhary/Desktop/Expensio");
+
+        List<CodeChunk> store = new ArrayList<>();
+
+        for(String[] chunk : chunks){
+            String filename = chunk[0];
+            String content = chunk[1];
+
+            List<Double> vector = EmbeddingExample.getEmbedding(content);
+            store.add(new CodeChunk(content, vector, filename));
+        }
+        System.out.println("Totak chunks stored: " + store.size());
+        System.out.println("First chunk file: " + store.get(0).filename);
+        System.out.println("Vector length: " + store.get(0).embedding.size());
+    }
+}
