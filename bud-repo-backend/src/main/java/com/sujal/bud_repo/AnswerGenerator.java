8b406018c14843d644746eafeb597f6ce486b660
Sujal Choudhary <chsujal8@gmail.com>
2026-04-21 00:15:13 +0530

    created skeleton class for answer generation

---
+++ bud-repo-backend/src/main/java/com/sujal/bud_repo/AnswerGenerator.java
@@ -0,0 +1,9 @@
+package com.sujal.bud_repo;
+
+import java.util.List;
+
+public class AnswerGenerator {
+    public static String answerGenerator(String question, List<CodeChunk> relevantChunks) throws Exception{
+
+    }
+}
